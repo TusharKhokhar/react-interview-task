3a493f30a1d27b131c9841cd30eecae03863984d
TusharKhokhar <khokhartushar15@gmail.com>
2023-05-24 01:05:44 +0530

    init

---
+++ src/pages/AddProduct.tsx
@@ -0,0 +1,179 @@
+import { useState } from "react";
+import "./AddProducts.css";
+import { Alert, AlertColor } from "@mui/material";
+
+const AddProduct = () => {
+  const initialState = {
+    productName: "",
+    productPrice: "",
+    productPhoto: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  const [error, setError] = useState<any>(null);
+  const [showAlert, setShowAlert] = useState<{
+    message: string;
+    type: AlertColor | undefined;
+  }>({
+    message: "",
+    type: undefined,
+  });
+
+  function getExtension(fileName: string) {
+    return /[.]/.exec(fileName)
+      ? /[^.]+$/.exec(fileName)?.[0].toLowerCase()
+      : undefined;
+  }
+
+  const onChange = (e: any) => {
+    if (e.target.name === "productPhoto") {
+      const ALLOWED_FILE_EXTENSIONS = ["jpg", "jpeg", "png"];
+      const extension: any = getExtension(e.target.files[0].name);
+      if (ALLOWED_FILE_EXTENSIONS.indexOf(extension) === -1) {
+        setError({
+          has_error: true,
+          message:
+            "Invalid file type. (supported file types are .jpg, .jpeg and .png)",
+          field: "productPhoto",
+        });
+      }
+    } else if (e.target.name === "productPrice") {
+      const regex = /^[0-9\b]+$/;
+      if (regex.test(e.target.value) || e.target.value.trim().length === 0) {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value.trim().length
+            ? parseInt(e.target.value)
+            : "",
+        });
+      }
+    } else {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const validateForm = () => {
+    if (!formData.productName.trim().length) {
+      return {
+        has_error: true,
+        message: "This is required field",
+        field: "productName",
+      };
+    }
+    if (!formData.productPrice) {
+      return {
+        has_error: true,
+        message: "This is required field",
+        field: "productPrice",
+      };
+    }
+    if (!formData.productPhoto) {
+      return {
+        has_error: true,
+        message: "This is required field",
+        field: "productPhoto",
+      };
+    }
+
+    return { has_error: false, error: null, field: null };
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const { has_error, message, field }: any = validateForm();
+    if (has_error) {
+      setError({ message, field });
+      return;
+    }
+    setShowAlert({
+      message: "Product added successfully",
+      type: "success",
+    });
+    setFormData(initialState);
+    setTimeout(() => {
+      setShowAlert({
+        message: "",
+        type: undefined,
+      });
+    }, 3000);
+  };
+
+  return (
+    <div>
+      {showAlert.message && (
+        <Alert
+          severity={showAlert.type}
+          onClose={() =>
+            setShowAlert({
+              message: "",
+              type: undefined,
+            })
+          }
+        >
+          {showAlert.message}
+        </Alert>
+      )}
+      <h2>Add Product</h2>
+      <form>
+        <div className="form-fieldset">
+          <label className="form-label" htmlFor="productName">
+            Product Name
+          </label>
+          <input
+            placeholder="Enter Product Name"
+            className="form-input"
+            id="productName"
+            type="text"
+            name="productName"
+            value={formData.productName}
+            onChange={onChange}
+          />
+          {error && error.field && error.field === "productName" && (
+            <span className="error-field">{error.message}</span>
+          )}
+        </div>
+        <div className="form-fieldset">
+          <label className="form-label" htmlFor="productPrice">
+            Product Price
+          </label>
+          <input
+            placeholder="Enter Product Price"
+            className="form-input"
+            id="productPrice"
+            type="text"
+            name="productPrice"
+            value={formData.productPrice}
+            onChange={onChange}
+          />
+          {error && error.field && error.field === "productPrice" && (
+            <span className="error-field">{error.message}</span>
+          )}
+        </div>
+        <div className="form-fieldset">
+          <label className="form-label" htmlFor="productPhoto">
+            Product Photo
+          </label>
+          <input
+            type="file"
+            id="productPhoto"
+            name="productPhoto"
+            accept="image/*"
+            value={formData.productPhoto}
+            onChange={onChange}
+          />
+          {error && error.field && error.field === "productPhoto" && (
+            <span className="error-field">{error.message}</span>
+          )}
+        </div>
+        <button className="submit-btn" type="button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProduct;
---
+++ src/pages/Products.tsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import CustomTable from "../components/Table/Table";
+import { formatDate, padNumber } from "../components/Table/utils";
+import "./Products.css";
+
+function Products() {
+  const rowsPerPage = 10;
+  const [tablePage, setTablePage] = useState(1);
+  const [tableOrder, setTableOrder] = useState({
+    order: "asc",
+    orderBy: "createdAt",
+  });
+
+  const tableHeader = [
+    {
+      id: "id",
+      numeric: false,
+      disablePadding: false,
+      label: "S. No.",
+      render: (text: any, row: any, index: any, data: any) =>
+        padNumber(index + 1 + rowsPerPage * (tablePage - 1), 1),
+    },
+    {
+      id: "name",
+      numeric: false,
+      disablePadding: false,
+      label: "Product Name",
+      sortable: true,
+    },
+    {
+      id: "price",
+      numeric: false,
+      disablePadding: false,
+      label: "Price",
+      sortable: false,
+    },
+    {
+      id: "image",
+      numeric: false,
+      disablePadding: false,
+      label: "Image",
+      sortable: false,
+      render: (text: any, row: any, index: any, data: any) => (
+        <img src={row.image} className="product-image" />
+      ),
+    },
+    {
+      id: "createdAt",
+      numeric: false,
+      disablePadding: false,
+      label: "Created At",
+      sortable: false,
+      render: (text: any) => formatDate(text),
+    },
+  ];
+
+  const tableData = [
+    {
+      id: "1",
+      name: "Test123",
+      price: "120",
+      image:
+        "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      createdAt: new Date(),
+    },
+    {
+      id: "1",
+      name: "Test123",
+      price: "1204",
+      image:
+        "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      createdAt: new Date(),
+    },
+  ];
+
+  const handlePageChange = (page: number) => {
+    setTablePage(page);
+  };
+
+  const handleSortChange = (order: string, orderBy: string) => {
+    setTableOrder({ order, orderBy });
+  };
+
+  return (
+    <CustomTable
+      tableHeader={tableHeader}
+      tableData={tableData}
+      totalRecords={tableData.length}
+      sortingRequired={true}
+      paginationServerSide={true}
+      page={tablePage}
+      rowsPerPage={rowsPerPage}
+      onPageChange={handlePageChange}
+      sortingServerSide={true}
+      order={tableOrder.order}
+      orderBy={tableOrder.orderBy}
+      onSortChange={handleSortChange}
+      rowclickable={false}
+      isLoading={false}
+      paginationRequired={true}
+    />
+  );
+}
+
+export default Products;
